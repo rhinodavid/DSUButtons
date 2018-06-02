db74f5b1bf46bedf1b9e30fd3da4b24c0f006451
arne182 <arneschwarck@gmail.com>
2018-06-02 22:07:58 +0200

    Returned back to JoelID and added custom settings

--- Arduino/DSUButtons/DSUButtons.ino
+++ Arduino/DSUButtons/DSUButtons.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
-#include <mcp_can.h>
+#include "mcp_can.h"
+
 
 //IO Pins
 const int fdistance = 3;
@@ -9,13 +10,14 @@ const int relay2 = 6;
 
 //CAN
 const int SPI_CS_PIN = 10;
-unsigned char flagRecv = 1;
+unsigned char flagRecv = 0;
 unsigned char len = 3;
 unsigned char buf[8];
 unsigned long interval=100; // the time we need to wait
 unsigned long previousMillis=0; // millis() returns an unsigned long.
-unsigned char stmp[5] = {0, 0, 0, 0, 0};
-unsigned char JOEL_ID[2] = {0, 0};
+//unsigned char stmp[5] = {0, 0, 0, 0, 0};
+unsigned char JOEL_ID[2] = {0x02, 0x00};
+unsigned char stmp[8] = {0x00, 0x64, 0x00, 0x28, 0x00, 0x00, 0x00, 0x00};
 unsigned char NEW_MSG_1[8] = {0};
 MCP_CAN CAN(SPI_CS_PIN);
 int canSendId = 0x203;
@@ -25,8 +27,8 @@ int fdistanceState = 1;
 int lanewarnState = 1;
 int lane100mscount = 0;
 int dist100mscount = 0;
-int relaystate1 = 1; // 1 = DSU off
-int relaystate2 = 1; // 1 = remote off
+int relaystate1 = 1; // 0 = DSU off
+int relaystate2 = 1; // 0 = remote off
 
 ////SERIAL INPUT (UNUSED)
 //char receivedChar;
@@ -34,7 +36,7 @@ int relaystate2 = 1; // 1 = remote off
 void setup()
 { 
   //GPIO
-  pinMode(fdistance, INPUT);
+  pinMode(fdistance, INPUT_PULLUP);
   pinMode(lanewarn, INPUT_PULLUP);
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
@@ -48,9 +50,18 @@ void setup()
  //SPI (CAN)
   SPI.begin();  
   SPI.setClockDivider(SPI_CLOCK_DIV2);
-  
+  delay(1000);
+  delay(1000);
+  delay(1000);
+  delay(1000);
+  delay(1000);
+  delay(1000);
+  delay(1000);
+  delay(1000);
+  delay(1000);
+  delay(1000);  
  //CAN
-    while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 500                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            00k
+    while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 1000                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            00k
     {
         Serial.println("CAN BUS Shield init fail");
         Serial.println(" Init CAN BUS Shield again");
@@ -59,14 +70,15 @@ void setup()
     Serial.println("CAN BUS Shield init ok!");
 
 //  attachInterrupt(2, MCP2515_ISR, FALLING); // start interrupt for can
-  CAN.init_Mask(0, 0, 0x3ff); // there are 2 mask in mcp2515, you need to set both of them
-  CAN.init_Mask(1, 0, 0x3ff);
-  CAN.init_Filt(0, 0, 0x2c1);
-  CAN.init_Filt(1, 0, 0x2c1);
-  CAN.init_Filt(2, 0, 0x2c1);
-  CAN.init_Filt(3, 0, 0x2c1);
-  CAN.init_Filt(4, 0, 0x2c1);
-  CAN.init_Filt(5, 0, 0x2c1);
+  CAN.init_Mask(0, 0, 0xFFF); // there are 2 mask in mcp2515, you need to set both of them
+  CAN.init_Mask(1, 0, 0xFFF);
+  CAN.init_Filt(0, 0, 0xFFF);
+  CAN.init_Filt(1, 0, 0xFFF);
+  CAN.init_Filt(2, 0, 0xFFF);
+  CAN.init_Filt(3, 0, 0xFFF);
+  CAN.init_Filt(4, 0, 0xFFF);
+  CAN.init_Filt(5, 0, 0xFFF);
+
 }
 
 ////SERIAL INPUT (unused)
@@ -91,29 +103,44 @@ void loop()
   if ((unsigned long)(currentMillis - previousMillis) >= interval) {
 
     if (fdistanceState == LOW) {
-      JOEL_ID[0] = 0b10000000;
       dist100mscount++;
     } else {
-      JOEL_ID[0] = 0b00000000;
       dist100mscount = 0;
     }
-
-
+    if ((fdistanceState == LOW) && (dist100mscount > 2)) {
+       if (JOEL_ID[0] == 0x02)
+       {
+          JOEL_ID[0] = 0x03;
+          dist100mscount = 0;
+       }
+       else if (JOEL_ID[0] == 0x03)
+       {
+          JOEL_ID[0] = 0x01;
+          dist100mscount = 0;
+       }
+       else if (JOEL_ID[0] == 0x01)
+       {
+          JOEL_ID[0] = 0x02;
+          dist100mscount = 0;
+       }
+    }
     if (lanewarnState == LOW) {
-      JOEL_ID[1] = 0b10000000;
+      JOEL_ID[1] = 0x01;
       lane100mscount++;
     } else {
-      JOEL_ID[1] = 0b00000000;
+      JOEL_ID[1] = 0x00;
       lane100mscount = 0;
     }
-    //CAN.sendMsgBuf(canSendId, 0, 2, JOEL_ID);
+    CAN.sendMsgBuf(canSendId, 0, 2, JOEL_ID);
     //Serial.println(dist100mscount);
     if ((fdistanceState == LOW) && (dist100mscount > 50)) {
+      //CAN.sendMsgBuf(canSendId, 0, 2, stmp);
       dist100mscount = 0;
       if (relaystate1 == 0){
         relaystate1 = 1;
         Serial.println("relay1=1");
         digitalWrite(relay1, relaystate1);
+        
       }
       else if (relaystate1 == 1) {
         relaystate1 = 0;
@@ -121,19 +148,27 @@ void loop()
         digitalWrite(relay1, relaystate1);
       }
     }
-    if ((lanewarnState == LOW) && (lane100mscount > 50)) {
+    if ((lanewarnState == LOW) && (lane100mscount = 10)) {
+      //CAN.sendMsgBuf(canSendId, 0, 2, stmp);
       lane100mscount = 0;
       if (relaystate2 == 0){
         relaystate2 = 1;
         Serial.println("relay2=1");
         digitalWrite(relay2, relaystate2);
+        
       }
-      else if (relaystate == 1) {
+      else if (relaystate2 == 1) {
         relaystate2 = 0;
         Serial.println("relay2=0");
         digitalWrite(relay2, relaystate2);
       }
+    else if ((lanewarnState == HIGH) && (relaystate2 == 1))
+        relaystate2 = 0;
+        lane100mscount = 0;
+        Serial.println("relay2=0");
+        digitalWrite(relay2, relaystate2);
     }
+
     previousMillis = millis();
   }
 
